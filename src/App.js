e30e6f958ae7ae598e1b7d142758eb63874dfdce
SandeepKushwaha <sandeepkush327@gmail.com>
2023-09-02 11:52:16 +0530

    accessing variable on react component

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let message = 'Hello React.';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {message} 
+      </div>
     </div>
   );
 }
